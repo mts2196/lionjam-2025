64b90f9eb6ee4a6aab332ecb98dabf1dbf314185
aaiden2 <108093442+aaiden2@users.noreply.github.com>
2025-02-23 23:31:36 -0500

    winning thing

---
+++ area_2d.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var win_sound: AudioStreamPlayer
+@export var win_image: Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	win_image.visible = false # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	if body.name == "Elon Pusk":  # Check if the player's name is Elon Pusk
+		# Play the sound
+		win_sound.play()
+
+		# Show the "Win" image
+		win_image.visible = true
