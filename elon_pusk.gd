1af88e18e9ad30165ad7feda4000af9e781f5aed
Enoch Sanchez <ehboy604@gmail.com>
2025-02-24 00:38:14 -0500

    yeppers

--- elon_pusk.gd
+++ elon_pusk.gd
@@ -3,12 +3,18 @@ extends CharacterBody2D
 @export var speed: float = 50.0  # Player movement speed
 
 @onready var sprite = $AnimatedSprite2D
+@onready var footsteps = $walking
+@onready var music = $backmusic
+var is_moving = false
+
+func _ready():
+	music.play()
 
 var direction = Vector2.ZERO
 func _physics_process(delta):
 	# Reset direction each frame
 	direction = Vector2.ZERO
-
+	
 	# Handle movement input
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1 
@@ -29,6 +35,14 @@ func _physics_process(delta):
 	# Normalize direction for consistent speed (diagonal movement is not faster)
 	direction = direction.normalized()
 	
+	if direction.length() > 0:
+		if not is_moving:
+			is_moving = true
+			footsteps.play()
+	else:
+		if is_moving:
+			is_moving = false
+			footsteps.stop()
 	
 	# Set the velocity to move the character in the specified direction
 	velocity = direction * speed
